0d809b84dda7dd086b97a7016ea9b13c4b584179
AZWaterWarriors <153463281+AZWaterWarriors@users.noreply.github.com>
2023-12-29 09:34:38 -0700

    Create measure.h

---
+++ measure.h
@@ -0,0 +1,16 @@
+/*
+ *
+ * Water Warriors Moisture Harvester Meteorological Measurement System
+ *
+ */
+
+#ifndef _MEASURE_H
+#define _MEASURE_H
+
+#define SENSOR_PIN
+
+int initsensor(void);
+float gettemp(void);
+float gethumid(void);
+
+#endif
