ca20d0960040b6bae0d6267570b4def0e0f5f5ae
AZWaterWarriors <153463281+AZWaterWarriors@users.noreply.github.com>
2023-12-29 09:33:30 -0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,27 @@
+/*
+ *
+ * Water Warriors Moisture Harvester Meteorological Measurement System
+ *
+ */
+
+#include <stdio.h>
+
+#include "measure.h"
+#include "rpigpio/electronics.h"
+
+int initsensor(void);
+float gettemp(void);
+float gethumid(void);
+
+int main(int argc, char *argv[]){
+
+  setupio();
+
+  if(initsensor() != 0){ return -1; };
+
+	for(;;){
+    float temp = gettemp();
+    float humid = gethumid();
+  };
+
+};
